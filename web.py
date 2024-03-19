908d13fe7fb02a33a173748a2f114ead575e13b9
Arshdeep Singh <114864356+ArshHanjra@users.noreply.github.com>
2024-03-19 16:40:08 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,14 @@
+import sys 
+import requests
+from bs4 import BeautifulSoup
+url=sys.argv[1]
+
+resp=requests.get(url,verify=False)
+if resp.status_code==200:
+    print("Success")
+    soup=BeautifulSoup(resp.text,"html.parser")
+    l=soup.find()
+    print(l.text)
+
+else:
+    print("error")
